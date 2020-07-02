5eb184f2a2925354f12146e9f2b37585cb8b0f6a
Becca <rrdoukas@gmail.com>
2020-07-02 17:13:24 -0500

    adds homework

---
+++ src/views/Home.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="home">
+    <h1>New Movie</h1>
+    <ul>
+      <li v-for="error in createErrors">{{ error }}</li>
+    </ul>
+
+    <div>
+      Title: <input type="text" v-model="newTitle"> <br>
+      Year: <input type="integer" v-model="newYear"> <br>
+      Plot: <input type="text" v-model="newPlot"> <br>
+      Director: <input type="text" v-model="newDirector"> <br>
+      <button v-on:click="createMovie()">Create</button>
+    </div>
+
+    <div v-for="movie in movies">
+      <h2>{{ movie.title }}</h2>
+      <p>Year: {{ movie.year }}</p>
+      <p>Plot: {{ movie.plot }}</p>
+      <p>Director: {{ movie.director }}</p>
+      <button v-on:click="showMovie(movie)">More Info</button>
+    </div>
+
+    <dialog id="movie-details">
+      <form method="dialog">
+        <h1>Movie Info</h1>
+        <!-- <ul>
+          <li v-for="error in updateErrors">{{ error }}</li>
+        </ul> -->
+        <p>Title: <input type="text" v-model="currentMovie.title"></p>
+        <p>Year: <input type="integer" v-model="currentMovie.year"></p>
+        <p>Plot: <input type="text" v-model="currentMovie.plot"></p>
+        <p>Director : <input type="text" v-model="currentMovie.director"></p>
+        <button v-on:click="updateMovie(currentMovie)">Update</button>
+        <button v-on:click="destroyMovie(currentMovie)">Delete</button>
+        <button>Close</button>
+      </form>
+    </dialog>
+
+  </div>
+</template>
+
+<style>
+img {
+  width: 250px;
+}
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      movies: [],
+      createErrors: [],
+      // updateErrors: [],
+      newTitle: "",
+      newYear: "",
+      newPlot: "",
+      newDirector: "",
+      currentMovie: {}
+    };
+  },
+  created: function() {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function() {
+      axios.get("/api/movies").then(response => {
+        console.log("All Movies:", response.data);
+        this.movies = response.data;
+      });
+    },
+    createMovie: function() {
+      var params = {
+        title: this.newTitle,
+        plot: this.newPlot,
+        year: this.newYear,
+        director: this.newDirector
+      };
+      axios
+        .post("/api/movies", params)
+        .then(response => {
+          console.log("Success", response.data);
+          this.movies.push(response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+          this.createErrors = error.response.data.errors;
+        });
+    },
+    showMovie: function(movie) {
+      console.log(movie);
+      this.currentMovie = movie;
+      document.querySelector("#movie-details").showModal();
+    },
+    updateMovie: function(movie) {
+      var params = {
+        title: movie.title,
+        plot: movie.plot,
+        year: movie.year,
+        director: movie.director
+      };
+      axios
+        .patch(`/api/movies/${movie.id}`, params)
+        .then(response => {
+          console.log("Successfully Updated", response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+        });
+    },
+    destroyMovie: function(movie) {
+      axios.delete(`/api/movies/${movie.id}`).then(response => {
+        console.log("Successfully destroyed", response.data);
+        var index = this.movies.indexOf(movie);
+        this.movies.splice(index, 1);
+      });
+    }
+  }
+};
+</script>
