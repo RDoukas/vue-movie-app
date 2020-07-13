7ef00f8e2c09c3cd3ebf2e7e611771af50f9d586
Becca <rrdoukas@gmail.com>
2020-07-13 17:55:26 -0500

    updates homework

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,39 @@
 <template>
+
   <div class="home">
-    <h1>New Movie</h1>
+    <div class="col s12 m7">
+      <h2 class="header">All Actors</h2>
+      <div class="card horizontal">
+        <div class="card-image">
+          <img src="images/billy.jpg">
+        </div>
+        <div class="card-stacked">
+          <div class="card-content">
+            <p>Here's a list of all actors.</p>
+          </div>
+          <div class="card-action">
+            <a href="/actors">Click here!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m7">
+      <h2 class="header">All Movies</h2>
+      <div class="card horizontal">
+        <div class="card-image">
+          <img src="images/dazed-and-confused.jpeg">
+        </div>
+        <div class="card-stacked">
+          <div class="card-content">
+            <p>Here's a list of all actors.</p>
+          </div>
+          <div class="card-action">
+            <a href="/movies">Click here!</a>
+          </div>
+      </div>
+    </div>
+  </div>
+    <!-- <h1>New Movie</h1>
     <ul>
       <li v-for="error in createErrors">{{ error }}</li>
     </ul>
@@ -11,9 +44,9 @@
       Plot: <input type="text" v-model="newPlot" /> <br />
       Director: <input type="text" v-model="newDirector" /> <br />
       <button v-on:click="createMovie()">Create</button>
-    </div>
+    </div> -->
 
-    <h1>New Actor</h1>
+    <!-- <h1>New Actor</h1>
     <ul>
       <li v-for="error in createErrors">{{ error }}</li>
     </ul>
@@ -25,13 +58,20 @@
       Gender: <input type="text" v-model="newGender" /> <br />
       Age: <input type="text" v-model="newAge" /> <br />
       <button v-on:click="createActor()">Create</button>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <style>
 img {
   width: 250px;
+}
+.home {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
+  padding: 16px;
+  text-align: center;
+  width: 320px;
+  /* background-color: #f1f1f1; */
 }
 </style>
 
@@ -54,7 +94,7 @@ export default {
       newKnownFor: "",
       newGender: "",
       newAge: "",
-      currentActor: {},
+      currentActor: {}
     };
   },
   created: function() {
@@ -62,7 +102,7 @@ export default {
   },
   methods: {
     indexMovies: function() {
-      axios.get("/api/movies").then((response) => {
+      axios.get("/api/movies").then(response => {
         console.log("All Movies:", response.data);
         this.movies = response.data;
       });
@@ -72,15 +112,15 @@ export default {
         title: this.newTitle,
         year: this.newYear,
         plot: this.newPlot,
-        director: this.newDirector,
+        director: this.newDirector
       };
       axios
         .post("/api/movies", params)
-        .then((response) => {
+        .then(response => {
           console.log("Success", response.data);
           this.movies.push(response.data);
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error.response.data.errors);
           this.createErrors = error.response.data.errors;
         });
@@ -95,24 +135,24 @@ export default {
         title: movie.title,
         year: movie.year,
         plot: movie.plot,
-        director: movie.director,
+        director: movie.director
       };
       axios
         .patch(`/api/movies/${movie.id}`, params)
-        .then((response) => {
+        .then(response => {
           console.log("Successfully Updated", response.data);
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error.response.data.errors);
         });
     },
     destroyMovie: function(movie) {
-      axios.delete(`/api/movies/${movie.id}`).then((response) => {
+      axios.delete(`/api/movies/${movie.id}`).then(response => {
         console.log("Successfully destroyed", response.data);
         var index = this.movies.indexOf(movie);
         this.movies.splice(index, 1);
       });
-    },
-  },
+    }
+  }
 };
 </script>
